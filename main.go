6f87289a5fc1fc4e15f2b8ce5c1a9bf12eaf6c8e
alexanderch <chesnoalex@gmail.com>
2020-01-31 12:42:32 +1100

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World! It's me!!!!}
+		res := &response{Message: "Hello World! It's me!!!!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
